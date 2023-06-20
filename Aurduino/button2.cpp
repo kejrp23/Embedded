73d3dcf703015f46ca5e81bc792937733f7e87ae
Jason R. Pittman <j.pittman51@att.net>
2023-06-19 21:04:02 -0400

    Create button2.cpp
    ranged for loops

---
+++ Aurduino/button2.cpp
@@ -0,0 +1,40 @@
+// C++ code
+//
+const int buttonpin {2};
+int ledpin[3] = {12,13,11};
+int ledstate = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(buttonpin, INPUT_PULLUP);
+  for (int pin : ledpin){
+    	pinMode(pin, OUTPUT);
+  }
+  
+}
+
+void loop(){
+  //Serial.println(digitalRead(buttonpin));
+  int buttonstate = digitalRead(buttonpin);
+  
+  if (ledstate == 1 && buttonstate == 0){
+    for (int pin : ledpin){
+   		digitalWrite(pin,LOW);
+    }
+    Serial.println("Off");
+    ledstate = 0;
+    delay(500);
+  }
+  else if (ledstate == 0 && buttonstate == 0){
+    for (int pin : ledpin){
+      digitalWrite(pin, HIGH);
+    }
+   ledstate = 1;
+   Serial.println("ON");
+    delay(500);
+   
+  };
+  
+}
+
