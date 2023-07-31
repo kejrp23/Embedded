aed29da4bc8b1029334cdc96176f0333a27aa42a
kejrp23 <j.pittman51@att.net>
2023-07-30 21:47:49 -0400

    added on/off functionality

--- Arduino/My Headers/devices.h
+++ Arduino/My Headers/devices.h
@@ -10,4 +10,19 @@ struct device {
 
     device(int pin_number, int data)
       		:pin{pin_number}, datapin{data}{};
+
+
+
+	void on(){
+			if (last_status == 0){ digitalWrite(pin,HIGH)}
+
+			else if (last_status == 1){}};
+
+	void off(){
+			if (last_status == 1){ digitalWrite(pin,LOW)}
+
+			else if (last_status == 1){}};
+
+
+
 };
