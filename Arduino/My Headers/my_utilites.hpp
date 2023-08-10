39be3a5a4543dfcf137a19f674d29ca25a40e113
kejrp23 <j.pittman51@att.net>
2023-08-09 22:19:38 -0400

    added utility file

---
+++ Arduino/My Headers/my_utilites.hpp
@@ -0,0 +1,21 @@
+
+
+template<typename T>
+void error(T x){
+		Serial.print("An Error has occurred in: ");
+		Serial.println(x);
+};
+
+
+template<typename T>
+void error(T msg, T x){
+		Serial.println(msg,x);
+};
+
+template<typename T>
+void print(T x){
+		Serial.println(x);
+};
+
+
+
