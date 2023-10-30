dc30ec6e2223b2808d9836f01b3557f9e837b999
Kejrp23 <j.pittman51@att.net>
2023-10-29 22:50:54 -0400

    Updated formatting, comments, and removed unused function

--- Arduino/My Headers/my_utilites.hpp
+++ Arduino/My Headers/my_utilites.hpp
@@ -1,27 +1,25 @@
-
+//This file is to contain useful function's and templates to make every day embedded programming easier. 
 
 template<typename T>
-void error(T x){
+void error(T errornumber){
 		Serial.print("An Error has occurred in: ");
-		Serial.println(x);
+		Serial.println(errornumber);
 };
 
+
+//not sure this would work as intended, revisit.
 
 template<typename T>
-void error(T msg, T x){
-		Serial.println(msg,x);
+void error(T msg, T errornumber){
+		Serial.println(msg,errornumber);
 };
+
+
 
 template<typename T>
 void println(T x){
 		Serial.println(x);
 };
-
-template<typename T>
-void print(T x){
-		Serial.print(x);
-};
-
 
 
 
