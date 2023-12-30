b4356b58a4ad5c1194e0d58fef8d18e512138c39
kejrp23 <j.pittman51@att.net>
2023-12-29 20:58:44 -0500

    updated water files

---
+++ Arduino/PlantWaterer/plantsystem.cpp
@@ -0,0 +1,9 @@
+
+#include "../My Headers/devices.h"
+
+
+
+
+
+
+
---
+++ Arduino/watersystem/watersystem.ino
@@ -0,0 +1,16 @@
+#include "devices.h"
+
+device pump(3);
+device button(7,7);
+device led(9);
+
+
+
+void setup() {
+
+
+}
+
+void loop() {
+
+}
