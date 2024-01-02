829f721789a88252494062792234d64e05e36fe8
kejrp23 <j.pittman51@att.net>
2024-01-02 12:43:45 -0500

    updated reverse logic for relays

--- Arduino/My Headers/devices.h
+++ Arduino/My Headers/devices.h
@@ -1,42 +1,61 @@
 
-struct device {
-    const int pin;
-    int datapin;
-	int response;
-    int last_status = 0;
+//Class to help control peripherals on Arduino board. User Friendly Interface.
 
-    device(int pin_number)
-			:pin{pin_number}{};
+class device {
+		public:
+			
+			const int pin;
+			int datapin;
+			int response;
+			int last_status = 0;
 
-    device(int pin_number, int data)
-      		:pin{pin_number}, datapin{data}{};
+			device(int pin_number)
+					:pin{pin_number}{};
 
-
-
-    void on(){
-        if (last_status == 0){ 
-          digitalWrite(pin,HIGH);
-          last_status = 1;
-          };
-    };
+			device(int pin_number, int data)
+					:pin{pin_number}, datapin{data}{};
 
 
-    void off(){
-        if (last_status == 1){
-          digitalWrite(pin,LOW);
-          last_status = 0;
-          };
-    };
 
-	int read(){
-			response = digitalRead(datapin);
-			return response;
-	};
+			void on(){
+				if (last_status == 0){ 
+				  digitalWrite(pin,HIGH);
+				  last_status = 1;
+				  };
+			};
 
-	void toggle(int time){
-			on();
-			delay(time);
-			off();
-			delay(time);
-	};
+
+			void off(){
+				if (last_status == 1){
+				  digitalWrite(pin,LOW);
+				  last_status = 0;
+				  };
+			};
+
+			int read(){
+					response = digitalRead(datapin);
+					return response;
+			};
+
+			void toggle(int time){
+					on();
+					delay(time);
+					off();
+					delay(time);
+			};
+
+
+			//controls for typical low power  relay switches are backwards
+			void relay_on(){
+					digitalWrite(pin,LOW);
+					last_status = 1;
+			};
+
+			void relay_off(){
+					digitalWrite(pin,HIGH);
+					last_status = 0;
+			};
+			//will expand this section for high power relay's if needed			
+		
 };
+
