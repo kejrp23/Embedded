b4f18acddeb389e6f69160d7af7fa739e39f119e
Kejrp23 <j.pittman51@att.net>
2024-01-10 08:29:04 -0500

    updated namespace usage, correcting spelling errors, and removed files

--- Arduino/My Headers/my_utilites.hpp
+++ Arduino/My Headers/my_utilites.hpp
@@ -1,27 +1,32 @@
 //This file is to contain useful function's and templates to make every day embedded programming easier. 
 
-template<typename T>
-void error(T errornumber){
-		Serial.print("An Error has occurred in: ");
-		Serial.println(errornumber);
+namespace utilities {
+
+
+		template<typename T>
+		void error(T errornumber){
+				Serial.print("An Error has occurred in: ");
+				Serial.println(errornumber);
+		};
+
+
+		//not sure this would work as intended, revisit.
+
+		template<typename T>
+		void error(T msg, T errornumber){
+				Serial.println(msg,errornumber);
+		};
+
+
+
+		template<typename T>
+		void println(T x){
+				Serial.println(x);
+		};
+
+		template<typename T>
+		void bitsizeof(T x){
+				std::cout << sizeof(x) * 8 << " bits" << '\n';
+		};
 };
 
-
-//not sure this would work as intended, revisit.
-
-template<typename T>
-void error(T msg, T errornumber){
-		Serial.println(msg,errornumber);
-};
-
-
-
-template<typename T>
-void println(T x){
-		Serial.println(x);
-};
-
-template<typename T>
-void bitsizeof(T x){
-		std::cout << sizeof(x) * 8 << " bits" << '\n';
-};
