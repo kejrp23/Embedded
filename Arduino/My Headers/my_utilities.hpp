eb051ba9688227fb574257eb8a1b420220a9324f
Kejrp23 <j.pittman51@att.net>
2024-01-09 21:43:52 -0500

    updated utilites

---
+++ Arduino/My Headers/my_utilities.hpp
@@ -0,0 +1,26 @@
+//This file is to contain useful function's and templates to make every day embedded programming easier. 
+
+template<typename T>
+void error(T errornumber){
+		Serial.print("An Error has occurred in: ");
+		Serial.println(errornumber);
+};
+
+
+//not sure this would work as intended, revisit.
+
+template<typename T>
+void error(T msg, T errornumber){
+		Serial.println(msg,errornumber);
+};
+
+
+
+template<typename T>
+void println(T x){
+		Serial.println(x);
+};
+
+
+
+
